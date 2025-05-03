2eaff6a7735e4eef8939c392e1afd4706a0b1d8c
khaled <khaledbahnawy@gmail.com>
2025-05-03 18:54:28 +0300

    make loading custom button and screen un usable when data is loading using AbsorbPointer

--- lib/widgets/custom_add_note_button.dart
+++ lib/widgets/custom_add_note_button.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add%20note%20cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add%20note%20cubit/add_note_state.dart';
 
 class CustomAddNoteButton extends StatelessWidget {
   const CustomAddNoteButton({super.key, required this.onTap});
@@ -15,13 +18,25 @@ class CustomAddNoteButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(12),
         ),
         child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
+          child: BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return state is AddNoteLoading
+                  ? SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ),
+                  )
+                  : Text(
+                    'Add',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+            },
           ),
         ),
       ),
