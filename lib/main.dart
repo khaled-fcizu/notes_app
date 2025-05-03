3d5e207520d70dc6250a295418269c0843cbc7a1
khaled <khaledbahnawy@gmail.com>
2025-05-03 13:47:49 +0300

    create observer to chick changes in states

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add%20note%20cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.openBox(kNoteBox);
   Hive.registerAdapter(NoteModelAdapter());
+  Bloc.observer = SimpleBlocObserver();
   runApp(const NotesApp());
 }
 
